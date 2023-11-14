8e8d2766730a74f7a7390d34d9a53b0ba3e9ab74
mihkuno <caindayjoeninyo@gmail.com>
2023-11-14 17:57:44 +0800

    refractored functions

--- main.js
+++ main.js
@@ -2,115 +2,81 @@
 import PDFParser from "pdf2json";
 
 
+const _pdf_to_textArray = (pdfData) => {
+    // select first page text data
+    const pageObject = pdfData['Pages'][0]['Texts'];
+
+    // iterate text object and append to text array     
+    const data = [];
+    for (const [index, textObject] of pageObject.entries()) {
+        const text = decodeURIComponent(textObject['R'][0]['T']);
+        data.push(text);
+    }
+    return data;
+}
+
+const _get_student_data = (data) => ({
+    // index < 32 is student data
+    id:              data[16],
+    name:            data[17],
+    campus:          data[1],
+    gender:          data[18],
+    age:             data[19],
+    college:         data[20],
+    department:      data[21],
+    major:           data[22],
+    year_level:      data[23],
+    curriculum:      data[24],
+    scholarship:     data[26],
+    nationality:     data[30],
+    contact:         data[31],
+    document_title:  data[0],
+    registration_no: data[15],
+    academic_year:   data[24],            
+    // index > 38 is schedules data
+})
+
+const _get_schedule_data = (data) => {
+    // all index greater than 38 is subject, units, schedules data
+    const schedules = [];
+    for (let i = 39; i < data.length; i += 8) {
+
+       // TODO: if i is a schedule, then append to previous object's schedule
+
+       // TODO: otherwise:
+
+        schedules.push(
+            {
+                instructor: data[i],
+                subject: data[i + 1],
+                lecture: data[i + 2],
+                laboratory: data[i + 3],
+                credit: data[i + 4],
+                schedule: data[i + 5],
+                code: data[i + 6],
+                section: data[i + 7],
+            }
+        );
+    }
+    return schedules;
+
+}
+
 const getStudentInfo = (filePath) => {
     
+    // create pdf parser instance
     const pdfParser = new PDFParser();
-
-    const assertTreeStructure = (pdfData) => {
-        const textData = pdfData['Pages'][0]['Texts'];
-
-        const studentInfo = [];
-        const scheduleContainer = [];
-        let schedule = {};
-        let counter = 0;
-
-
-        for (const [index, text] of textData.entries()) {
-            const decodeText = decodeURIComponent(text['R'][0]['T']);
-            
-            if      (index < 32) studentInfo.push(decodeText);
-            else if (index > 38) {
-
-                const sequence = [
-                    'instructor',
-                    'subject',
-                    'lecture',
-                    'laboratory',
-                    'credit',
-                    'datetime',
-                    'code',
-                    'section'
-                ];
-                schedule[sequence[counter]] = decodeText;
-                counter++;
-
-                // get row
-                if (counter > 7) {
-                    counter = 0;
-                    scheduleContainer.push(schedule);
-                    schedule = {};
-                } 
-
-                // TODO: check if row has multiple dates
-                 
-
-                // TODO: include total lab,lec,credit units
-                if (decodeText
-                        .toLowerCase()
-                        .startsWith('total unit')
-                ) { break; }
-            }
-        }
-
-
-
-        const studentInfoDict = {
-            id:              studentInfo[16],
-            name:            studentInfo[17],
-            campus:          studentInfo[1],
-            gender:          studentInfo[18],
-            age:             studentInfo[19],
-            college:         studentInfo[20],
-            department:      studentInfo[21],
-            major:           studentInfo[22],
-            year_level:      studentInfo[23],
-            curriculum:      studentInfo[24],
-            scholarship:     studentInfo[26],
-            nationality:     studentInfo[30],
-            contact:         studentInfo[31],
-            document_title:  studentInfo[0],
-            registration_no: studentInfo[15],
-            academic_year:   studentInfo[24],            
-            
-            // 32 > subject, units, schedules
-        }
-        
-
-        // temporary fixed-column indentation
-        let maxKeyLength = Math.max(...Object.keys(studentInfoDict).map(key => key.length));
-        for (let key in studentInfoDict) {
-            let spacing = ' '.repeat(maxKeyLength - key.length + 1);
-            console.log(key + ":" + spacing, studentInfoDict[key]);
-        }
-
-        for (let schedule of scheduleContainer) {
-            console.log('\n-----------------------------');
-            let maxKeyLength = Math.max(...Object.keys(schedule).map(key => key.length));
-            for (let key in schedule) {
-                let spacing = ' '.repeat(maxKeyLength - key.length + 1);
-                console.log(key + ":" + spacing, schedule[key]);
-            }
-            console.log('-----------------------------\n');
-        }
-
-        
-    }
     
-    pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
-    pdfParser.on("pdfParser_dataReady", pdfData => assertTreeStructure(pdfData));
+    // create functions handling data ready and errors
     pdfParser.loadPDF(filePath);
+    pdfParser.on("pdfParser_dataError", errData  => console.error(errData.parserError));
+    pdfParser.on("pdfParser_dataReady", pdfData => {
+        const data        = _pdf_to_textArray(pdfData);
+        const studentData = _get_student_data(data);
+        const scheduleData = _get_schedule_data(data);
+
+        console.log(scheduleData);
+    });
 }
 
 getStudentInfo("./cor.pdf");
-
-
-
-
-
-
-// fs.writeFile("output.json", jsonData, function(err) {
-//     if(err) {
-//         return console.log(err);
-//     }
-//     console.log("The file was saved!");
-// });
