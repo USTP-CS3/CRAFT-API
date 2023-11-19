ac8b6884246fc64c69a5f71a551287772ccc4192
mihkuno <caindayjoeninyo@gmail.com>
2023-11-19 15:40:25 +0800

    added docs to extractor

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 
-import { getCorInfo } from "./lib/extractor.js";
+import Extractor from "./lib/extractor.js";
 
 //
-// Main (self-executing async function)
+// Main (self-executing async function)--------------------------------------------------------------------------
 (async function main() {
     
     const filePath = "./misc/cor1.pdf";
 
     try {
-        const output = await getCorInfo(filePath);
+        const output    = await Extractor.getCorInfo(filePath);
         const structure = JSON.stringify(output, null, 4);
 
         console.log(structure);
