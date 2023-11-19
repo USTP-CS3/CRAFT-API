4311c8a48282766602436fc4c30b41f7809cf5ee
NathanJargon <metamorphiaSH@gmail.com>
2023-11-19 16:45:23 +0800

    scheduler

---
+++ lib/schedule.sql
@@ -0,0 +1,10 @@
+DELIMITER //
+
+CREATE PROCEDURE GetScheduleFrequency()
+BEGIN
+    SELECT scheduled_date, COUNT(DISTINCT student_id) as frequency
+    FROM schedule
+    GROUP BY scheduled_date;
+END //
+
+DELIMITER ;
--- main.js
+++ main.js
@@ -1,20 +1,92 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { getCorInfo } from './lib/extractor.js';
+import fs from 'fs';
+import path from 'path';
+import Database from "./lib/database.js";
+import Scheduler from "./lib/scheduler.js";
 
-import { getCorInfo } from "./lib/extractor.js";
+/**
+ * Logs a message to a file.
+ * 
+ * @param {string} message - The message to be logged.
+ * @param {string} [filename=log-datetime.log] - The name of the log file. Defaults to 'log-datetime.log'.
+ * @returns {void}
+ * @throws {Error} Throws an error if there is an issue writing to the log file.
+ */
 
-//
-// Main (self-executing async function)
+export function logToFile(message, filename = 'log-datetime.log') {
+    const currentFilePath = fileURLToPath(import.meta.url);
+
+    const logDirectory = path.join(dirname(currentFilePath), 'log');
+
+    if (!fs.existsSync(logDirectory)) {
+        fs.mkdirSync(logDirectory);
+    }
+
+    const logFilePath = path.join(logDirectory, filename);
+
+    fs.appendFileSync(logFilePath, `${message}\n`);
+}
+
+
+/**
+ * Main function to process the COR file, log the success or error.
+ * 
+ * @returns {void}
+ */
+
 (async function main() {
-    
-    const filePath = "./mics/cor3.pdf";
+    const filePath = './misc/cor3.pdf';
+
+    // Check if the file exists
+    if (!fs.existsSync(filePath)) {
+        const errorMessage = `Datetime: ${new Date().toISOString()}\nError: File does not exist`;
+        const logId = Math.floor(Math.random() * 1000);
+        logToFile(`${errorMessage}\nError Log ID: ${logId}`);
+        return;
+    }
+
+    // Check if the file extension is '.pdf'
+    if (!filePath.toLowerCase().endsWith('.pdf')) {
+        const errorMessage = `Datetime: ${new Date().toISOString()}\nError: File is not a PDF`;
+        const logId = Math.floor(Math.random() * 1000);
+        logToFile(`${errorMessage}\nError Log ID: ${logId}`);
+        return; 
+    }
 
     try {
         const output = await getCorInfo(filePath);
-        const structure = JSON.stringify(output, null, 4);
 
-        console.log(structure);
-    } 
-    catch (error) {
-        console.error("Error in main:", error);
+        // Check if the document title ends with 'CERTIFICATEOFREGISTRATION'
+        if (output && output.studentData && output.studentData.document_title === 'CERTIFICATEOFREGISTRATION') {
+            const structure = JSON.stringify(output, null, 4);
+            console.log(structure);
+            const successMessage = `Datetime: ${new Date().toISOString()}\nSuccess: ${structure}`;
+            logToFile(successMessage);
+        } else {
+            const errorMessage = `Datetime: ${new Date().toISOString()}\nError: Invalid document title`;
+            const logId = Math.floor(Math.random() * 1000);
+            logToFile(`${errorMessage}\nError Log ID: ${logId}`);
+        }
+    } catch (error) {
+        const errorMessage = `Datetime: ${new Date().toISOString()}\nError: ${error.message}`;
+        const logId = Math.floor(Math.random() * 1000);
+        logToFile(`${errorMessage}\nError Log ID: ${logId}`);
     }
-
 })();
+
+/*
+// change with MySQL user credentials
+const dbConfig = {
+  host: 'localhost',
+  user: 'your_test_user',
+  password: 'your_test_password',
+  database: 'test_database',
+};
+const yourDatabaseInstance = new Database(dbConfig);
+const scheduler = new Scheduler(yourDatabaseInstance);
+scheduler.displayFrequency()
+  .then(() => yourDatabaseInstance.close())
+  .catch((error) => console.error(`Error: ${error.message}`));
+*/
