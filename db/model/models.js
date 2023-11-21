3ac243b2ffe618d9cbee1eed46220d4b6cf48e91
Illamapalooza <juliusbaliling25@gmail.com>
2023-11-22 00:38:55 +0800

    compiled all models to export as 1 object

---
+++ db/model/models.js
@@ -0,0 +1,7 @@
+import Student from './Student.js';
+import Faculty from './Faculty.js';
+import Room from './Room.js';
+import Schedule from './Schedule.js';
+import Subject from './Subject.js';
+
+export { Student, Faculty, Room, Schedule, Subject };
