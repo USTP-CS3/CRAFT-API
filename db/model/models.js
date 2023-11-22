7cc37387867ff2f62c346f2d184b380abfaa326e
Joeninyo Cainday <joenino.cainday@ustp.edu.ph>
2023-11-22 10:44:50 +0800

    setup relationships

--- db/model/models.js
+++ db/model/models.js
@@ -4,4 +4,16 @@ import Room from './Room.js';
 import Schedule from './Schedule.js';
 import Subject from './Subject.js';
 
+Student.belongsToMany(Schedule, {through: 'StudentSchedule'});
+Schedule.belongsToMany(Student, {through: 'StudentSchedule'});
+
+Subject.hasMany(Schedule);
+Schedule.belongsTo(Subject);
+
+Faculty.hasMany(Schedule);
+Schedule.belongsTo(Faculty);
+
+Room.hasMany(Schedule);
+Schedule.belongsTo(Room);
+
 export { Student, Faculty, Room, Schedule, Subject };
