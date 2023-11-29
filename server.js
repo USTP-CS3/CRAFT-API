3b193b4a4f9c1a3a189096ea9f693c35b79d4ecd
Illamapalooza <juliusbaliling25@gmail.com>
2023-11-29 20:38:18 +0800

    added routes logic for requests

--- server.js
+++ server.js
@@ -1,59 +1,119 @@
-// import { synchronize } from '.server/db/database.js';
-
-// import Student  from './db/model/Student.js';
-// import Subject  from './db/model/Subject.js';
-// import Schedule from './db/model/Schedule.js';
-// import Room     from './db/model/Room.js';
-// import Faculty  from './db/model/Faculty.js';
-
-// import Extractor from "./lib/extractor.js";
-// import Logger    from "./lib/logger.js";
-
+import Student from './server/db/model/Student.js';
+import Subject from './server/db/model/Subject.js';
+import Schedule from './server/db/model/Schedule.js';
 import express from 'express';
 import { OAuth2Client } from 'google-auth-library';
-import { jwtDecode } from "jwt-decode";
+import { jwtDecode } from 'jwt-decode';
+import database from './server/db/database.js';
+import setup from './util/corUploader.js';
+import extractor from './server/lib/extractor.js';
+import formatData from './util/formatter.js';
 
 const app = express();
+const router = express.Router();
+
+// API routes
+app.use('/api', router); // Mount API routes at '/api' path
+app.use((err, req, res, next) => {
+ console.log(err);
+ res.status(500).send('Something went wrong!');
+});
 
 // --------------- everything here is public ---------------------
 // log all requests to a file (middleware)
 // -- morgan library --
 
-
-
 // --------------- everything here is private --------------------
 // verify token validity (middleware)
 // -- google oauth2 library --
-
-/api/student/
-// get self student data
-
-
-/api/student/setup
-// post student certificate of registration
-// middleware to extract student data from cor
-// middleware to check if student data exists
-// update if student data already exists
-
-
-/api/student/delete
-// delete self student data (non cascading)
-
-
-/api/schedule
-// get all schedules
-
-
-/api/subject
-// get all subjects
-
-
+// router.use(verifyToken);  --- this is the verifyToken middleware
 
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, async () => {
+ console.log(`Server is running on http://localhost:${PORT}`);
+ await database.connection();
 });
 
+// /api/student/
+// get self student data
+// router.get('/student', async (req, res) => {
+//  try {
+//   // Access user info from req.user
+//   const userId = req.user.sub; // or any other user-related info
+
+//   const student = await Student.findOne({ where: { id: userId } });
+
+//   if (!student) {
+//    return res.status(404).send('Student data not found');
+//   }
+
+//   res.json(student);
+//  } catch (error) {
+//   res.status(500).send('Internal Server Error');
+//  }
+// });
+
+// /api/student/setup
+// post student certificate of registration logic
+
+router.post('/setup', setup.array('files'), async (req, res) => {
+ const file = req.files;
+ console.log(file); // Log uploaded files information
+
+ // middleware to extract student data from cor
+ const studentData = await extractor.getCorInfo(file[0].path);
+
+ if (!studentData) {
+  return res.status(404).send('Student data not found');
+ }
+
+ //  format data to be inserted to database
+ const formattedData = formatData(studentData);
+
+ formattedData.studentData.email = 'sample@gmail.com'; //req.user.email  ;
+
+ await Student.upsert(formattedData.studentData);
+
+ res.status(200).json(req.files);
+});
+
+// /api/student/delete
+// delete self student data (non cascading)
+router.delete('/student/delete', async (req, res) => {
+ await Student.destroy({
+  where: {
+   id: req.user.id, // the id of the student to be deleted or EMAIL  req.user.email
+  },
+ })
+  .then(() => {
+   console.log('User deleted successfully.');
+  })
+  .catch((error) => {
+   console.error('Error occurred:', error);
+  });
+});
+
+// /api/schedule
+// get all schedules
+router.get('/schedule', async (req, res) => {
+ try {
+  const schedules = await Schedule.findAll();
+  res.json(schedules);
+ } catch (error) {
+  res.status(500).send('Internal Server Error');
+ }
+});
+
+// /api/subject
+// get all subjects
+router.get('/subject', async (req, res) => {
+ try {
+  const subjects = await Subject.findAll();
+  res.json(subjects);
+ } catch (error) {
+  res.status(500).send('Internal Server Error');
+ }
+});
 
 // app.use((req, res, next) => {
 //   console.log(req);
@@ -64,8 +124,7 @@ app.listen(PORT, () => {
 //   res.send('Hello World!');
 // });
 
-
- // try {
+// try {
 //   const authorization = req.headers.authorization;
 //   const user = jwtDecode(authorization);
 
@@ -73,17 +132,15 @@ app.listen(PORT, () => {
 //   const currentTime = Math.floor(Date.now() / 1000);
 //   const isExpired = currentTime > expireTime
 
-//   (isExpired) 
+//   (isExpired)
 //     ? res.status(401).json({message: 'Token expired'})
 //     : next();
 // }
-
 
 // (self-executing async function)--------------------------------------------------------------------------
 // (async function main() {
 
 //     const logger = Logger('./log/main.log')
-
 
 //     // extracts the info from the pdf file
 //     try {
@@ -92,18 +149,17 @@ app.listen(PORT, () => {
 //         const output    = await Extractor.getCorInfo("./misc/cor.pdf");
 //         const structure = JSON.stringify(output, null, 4);
 //         console.log(structure);
-//     } 
+//     }
 //     catch (error) {
 
 //         // TODO: add error handling and logging when COR extraction fails
 //         logger.append('');
 //         logger.append(error, 'ERROR');
 //     }
-    
 
 //     // puts the info to the database
 //     try {
-        
+
 //         // TODO: template for inserting data to the database
 //         const st = {
 //             first_name: 'Joxyle',
@@ -121,12 +177,12 @@ app.listen(PORT, () => {
 //         // create or update (constrained by unique attributes)
 //         const [student, isCreated] = await Student.upsert(st);
 
-//         (isCreated) 
+//         (isCreated)
 //             ? console.log('Student created:', student.toJSON())
 //             : console.log('Student updated:', student.toJSON());
 
 //         await synchronize();
-//     } 
+//     }
 //     catch (error) {
 //         // TODO: add error handling and logging when database sync fails
 //         console.error('Error creating or updating student:', error.message);
