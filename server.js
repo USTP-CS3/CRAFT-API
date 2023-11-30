91bd1bd103abdf0e9bc16dc9d8123fb6792e2152
mihkuno <caindayjoeninyo@gmail.com>
2023-11-30 20:45:54 +0800

    cleaned merge

--- server.js
+++ server.js
@@ -1,47 +1,22 @@
-import Student from './server/db/model/Student.js';
-import Subject from './server/db/model/Subject.js';
-import Schedule from './server/db/model/Schedule.js';
-
-import setup from './util/corUploader.js';
-import extractor from './server/lib/extractor.js';
-import formatData from './util/formatter.js';
 /**
- * 
- * handles all the requests and responses of the server 
- * 
+ *
+ * handles all the requests and responses of the server
+ *
  * req.craft object that contains the following:
  * - authenticated: boolean
  * - message: string
  * - account: object (contains the id token info)
  * - package: object (contains the route response)
- *  
+ *
  */
 
-import cors from 'cors';
-import express from 'express';
-import { Logger } from './server/routes/middleware/logger.js';
-import { Google } from './server/routes/middleware/google.js';
-import { StudentController } from './server/routes/controller/student.js';
+import cors from "cors";
+import express from "express";
+import { Logger } from "./server/routes/middleware/logger.js";
+import { Google } from "./server/routes/middleware/google.js";
+import { StudentController } from "./server/routes/controller/student.js";
 
 const app = express();
-const router = express.Router();
-
-// API routes
-app.use('/api', router); // Mount API routes at '/api' path
-app.use((err, req, res, next) => {
- console.log(err);
- res.status(500).send('Something went wrong!');
-});
-
-const PORT = 3000;
-app.listen(PORT, async () => {
- console.log(`Server is running on http://localhost:${PORT}`);
- await database.connection();
-});
-
-// /api/student/
-// get self student data
-
 
 /**
  * This is a proxy option for the dev server.
@@ -50,19 +25,25 @@ app.listen(PORT, async () => {
  */
 app.use(cors());
 
-
 /**
- * 
+ *
  * verify initializes the auth middleware and it creates the req.craft
  * listen reads the output of verify and logs all requests to database
  * auth is used by a specific route if it needs to be authenticated
- * 
+ *
  */
 app.use(Google.verify, Logger.listen);
 
 // get self student data
-app.get('/api/student', Google.auth, StudentController.get_data);
+app.get("/api/student", Google.auth, StudentController.get_data);
 
+const PORT = 5000;
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// ============================================================================================================
+// Template for creating a new route
 
 // /api/student/setup
 // post student certificate of registration
@@ -71,86 +52,75 @@ app.get('/api/student', Google.auth, StudentController.get_data);
 // update if student data already exists
 // app.post('/api/student/setup') {}
 
-
 //   res.json(student);
 //  } catch (error) {
 //   res.status(500).send('Internal Server Error');
 //  }
 // });
 
-
 // /api/student/setup
 // post student certificate of registration logic
-
+//
 // /api/student/delete
 // // delete self student data (non cascading)
 
-router.post('/setup', setup.array('files'), async (req, res) => {
- const file = req.files;
- console.log(file); // Log uploaded files information
+// router.post('/setup', setup.array('files'), async (req, res) => {
+//  const file = req.files;
+//  console.log(file); // Log uploaded files information
 
+// // middleware to extract student data from cor
+// const studentData = await extractor.getCorInfo(file[0].path);
 
-// middleware to extract student data from cor
-const studentData = await extractor.getCorInfo(file[0].path);
+// // /api/schedule
+// // // get all schedules
 
-// /api/schedule
-// // get all schedules
+//  if (!studentData) {
+//   return res.status(404).send('Student data not found');
+//  }
 
- if (!studentData) {
-  return res.status(404).send('Student data not found');
- }
-
- //  format data to be inserted to database
- const formattedData = formatData(studentData);
+//  //  format data to be inserted to database
+//  const formattedData = formatData(studentData);
 
 // /api/subject
 // // get all subjects
 
+// // /api/student/delete
+// // delete self student data (non cascading)
+// app.delete('/student/delete', async (req, res) => {
+//  await Student.destroy({
+//   where: {
+//    id: req.user.id, // the id of the student to be deleted or EMAIL  req.user.email
+//   },
+//  })
+//   .then(() => {
+//    console.log('User deleted successfully.');
+//   })
+//   .catch((error) => {
+//    console.error('Error occurred:', error);
+//   });
+// });
 
- formattedData.studentData.email = 'sample@gmail.com'; //req.user.email  ;
+// // /api/schedule
+// // get all schedules
+// app.get('/schedule', async (req, res) => {
+//  try {
+//   const schedules = await Schedule.findAll();
+//   res.json(schedules);
+//  } catch (error) {
+//   res.status(500).send('Internal Server Error');
+//  }
+// });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-// /api/student/delete
-// delete self student data (non cascading)
-app.delete('/student/delete', async (req, res) => {
- await Student.destroy({
-  where: {
-   id: req.user.id, // the id of the student to be deleted or EMAIL  req.user.email
-  },
- })
-  .then(() => {
-   console.log('User deleted successfully.');
-  })
-  .catch((error) => {
-   console.error('Error occurred:', error);
-  });
-});
-
-// /api/schedule
-// get all schedules
-app.get('/schedule', async (req, res) => {
- try {
-  const schedules = await Schedule.findAll();
-  res.json(schedules);
- } catch (error) {
-  res.status(500).send('Internal Server Error');
- }
-});
-
-// /api/subject
-// get all subjects
-app.get('/subject', async (req, res) => {
- try {
-  const subjects = await Subject.findAll();
-  res.json(subjects);
- } catch (error) {
-  res.status(500).send('Internal Server Error');
- }
-});
+// // /api/subject
+// // get all subjects
+// app.get('/subject', async (req, res) => {
+//  try {
+//   const subjects = await Subject.findAll();
+//   res.json(subjects);
+//  } catch (error) {
+//   res.status(500).send('Internal Server Error');
+//  }
+// });
 
 // app.use((req, res, next) => {
 //   console.log(req);
