c32b3d74fb2d4713c862e8edd5e35bf025bc2375
mihkuno <caindayjoeninyo@gmail.com>
2023-12-03 16:33:04 +0800

    responsive contributor text

--- client/src/components/Contributor/Contributor.tsx
+++ client/src/components/Contributor/Contributor.tsx
@@ -1,15 +1,31 @@
 import { useState } from 'react';
-import { Image, Flex, Container, Divider } from '@mantine/core';
-import { ResizeProvider } from '@/provider/ResizeProvider/ResizeProvider';
+import { Container, Center, Divider, Text } from '@mantine/core';
+import { ResizeProvider } from '../../provider/ResizeProvider/ResizeProvider';
 
 export function Contributor() {
+	const mobileWidthPx = 380;
+	const [isMobile, setIsMobile] = useState(window.innerWidth < mobileWidthPx);
+
+	const handleResize = (width: number) => {
+		window.innerWidth < mobileWidthPx ? setIsMobile(true) : setIsMobile(false);
+	};
+
 	return (
-		<Container size='xs' pt={0} pb={20}>
-			<Divider
-				my='xs'
-				label='Developed by the Computer Science Student Society (CS³) of USTP CDO'
-				labelPosition='center'
-			/>
-		</Container>
+		<ResizeProvider callback={handleResize}>
+			<Container size='xs' pt={0} pb={20}>
+				{isMobile ? (
+					<Center>
+						<Text c='dimmed' size='xs'>
+							Developed by the Computer Science Student Society (CS³)
+						</Text>
+					</Center>
+				) : (
+					<Divider
+						label='Developed by the Computer Science Student Society (CS³)'
+						labelPosition='center'
+					/>
+				)}
+			</Container>
+		</ResizeProvider>
 	);
 }
