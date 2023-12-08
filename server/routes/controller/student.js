5cdca5b8ab8b69852275c2fc77182c4a3a601013
mihkuno <caindayjoeninyo@gmail.com>
2023-12-08 14:59:37 +0800

    template import cor request

--- server/routes/controller/student.js
+++ server/routes/controller/student.js
@@ -17,65 +17,68 @@ function get_self_data(req, res) {
  * until the user agreed on the next request
  */
 
+// temporary storage for the extracted data from the pdf
 // import to database after the user agreed on the next request
-// temporary storage for the extracted data
 // the email of the user is the key of temp_cor_extract object
-// this contains the the file and data of the corpdf
-var temp_cor_extract = {};
+let temp_cor_extract = {};
 
-// TODO: save it to disk or perform other operations
+function reject(message, data = null) {
+	req.craft.message = message + error.message;
+	req.craft.package = data;
+	res.status(500).json(req.craft);
+}
 
 function post_extract_corpdf(req, res) {
 	try {
 		// Access the uploaded file from req.file
 		const corpdf = req.file.buffer;
 
+		const accept = (message, data) => {
+			req.craft.message = message;
+			req.craft.package = data;
+
+			// temporarily store the file and data with email as key
+			temp_cor_extract[req.craft.account.email] = {
+				file: corpdf,
+				data: req.craft.package,
+			};
+
+			res.status(200).json(req.craft);
+		};
+
 		Extractor.getCorInfo(corpdf)
 			.then(({ studentData, subjectData }) => {
 				// if document is not a Certificate of Registration, throw an error
-				if (studentData.document_title != 'CERTIFICATEOFREGISTRATION') {
-					throw new Error('Extraction Failed');
-				}
-
-				// if college is not CITC or campus is not USTP CDO,
-				// response with unsupported message
-				else if (
+				if (studentData.document_title != 'CERTIFICATE OF REGISTRATION') {
+					reject('Not A COR');
+					return;
+				} else if (
+					/**
+					 * if college is not CITC or campus is not USTP CDO,
+					 * response with unsupported message
+					 */
 					studentData.campus != 'USTP CDO CAMPUS' ||
 					studentData.college != 'COLLEGE OF INFORMATION TECHNOLOGY AND COMPUTING'
 				) {
-					req.craft.message = 'Unsupported College';
-					req.craft.package = { studentData, subjectData };
 					// TODO: save the file to misc/extract_unsupported folder
-					res.status(500).json(req.craft);
+					reject('Unsupported College', { studentData, subjectData });
 					return;
 				}
 
 				// if college is CITC, respond with a success message
 				else if (studentData.college == 'COLLEGE OF INFORMATION TECHNOLOGY AND COMPUTING') {
-					req.craft.message = 'Extraction Success';
-					req.craft.package = { studentData, subjectData };
-
-					// temporarily store the file and data with email as key
-					temp_cor_extract[req.craft.account.email] = {
-						file: corpdf,
-						data: req.craft.package,
-					};
-
-					res.status(200).json(req.craft);
+					accept('Extract Success', { studentData, subjectData });
 					return;
 				}
 			})
-			.catch((error) => {
-				req.craft.message = 'Extraction Failed: ' + error.message;
-				req.craft.package = null;
-				// TODO: save the file to misc/extract_failed folder
-				res.status(500).json(req.craft);
+			.catch(error => {
+				// TODO: save the file to misc/extract_failed folder for debugging
+				reject('Extract Failed');
 				return;
 			});
 	} catch (error) {
-		req.craft.message = 'Upload Failed' + error.message;
-		req.craft.package = null;
-		res.status(500).json(req.craft);
+		// TODO: save the file to misc/extract_failed folder for debugging
+		reject('Invalid Buffer');
 		return;
 	}
 }
@@ -84,6 +87,8 @@ function post_import_corpdf(req, res) {
 	// TODO: get the file and data from temp_cor_extract based on req.craft.account.email
 	// save the file to misc and import the data to the database
 	// if error, save the file to misc/failed_import folder
+	try {
+	} catch (error) {}
 }
 
 const StudentController = { get_self_data, post_extract_corpdf, post_import_corpdf };
